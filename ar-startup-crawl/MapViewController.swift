3625f7dce8b29ca1c7c2184375d9c497d1e8c099
Andrew Beers <apbeers@gmail.com>
2017-11-16 05:14:51 -0600

    fix location prompt

--- ar-startup-crawl/MapViewController.swift
+++ ar-startup-crawl/MapViewController.swift
@@ -55,7 +55,7 @@ class MapViewController: UIViewController, GMSMapViewDelegate, CLLocationManager
             print(error.localizedDescription)
         }
         
-        let camera = GMSCameraPosition.camera(withLatitude: 36.063610, longitude: -94.162561, zoom: 16)
+        let camera = GMSCameraPosition.camera(withLatitude: 36.063610, longitude: -94.162561, zoom: 15)
         mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         mapView.delegate = self
         locationManager.delegate = self
