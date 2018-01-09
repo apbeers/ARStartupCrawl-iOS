3710f732ae784e458a398fdca128a1eb0a625b9d
Andrew Beers <apbeers@gmail.com>
2018-01-09 15:11:55 -0600

    enable app groups

---
+++ ar-startup-crawl/WatchConnectivityHandler.swift
@@ -0,0 +1,13 @@
+//
+//  WatchConnectivityHandler.swift
+//  ar-startup-crawl
+//
+//  Created by Andrew Beers on 1/9/18.
+//  Copyright © 2018 Andrew Beers. All rights reserved.
+//
+
+import UIKit
+
+class WatchConnectivityHandler: NSObject {
+
+}
--- watch Extension/StartupsInterfaceController.swift
+++ watch Extension/StartupsInterfaceController.swift
@@ -8,10 +8,11 @@
 
 import WatchKit
 import Foundation
+import WatchConnectivity
 
+class StartupsInterfaceController: WKInterfaceController, WCSessionDelegate {
 
-class StartupsInterfaceController: WKInterfaceController {
-
+    var session: WCSession!
     
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
@@ -23,14 +24,32 @@ class StartupsInterfaceController: WKInterfaceController {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
         
-        
-        
+        session = WCSession.default
+        session.delegate = self
+        session.activate()
     }
-
     
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
-
+    
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        
+        switch activationState {
+        case WCSessionActivationState.activated:
+            print("session activated")
+        case WCSessionActivationState.notActivated:
+            print("session not activated")
+        default:
+            print("default case")
+        }
+        
+        session.sendMessage(["request", "startup_details"], replyHandler: { response in
+            print("reponse: \(response)")
+        }, errorHandler: { error in
+            print("error: \(error)")
+        })
+        
+    }
 }
