f451a18c236deed446aa537ffb86b8d5723395c9
Andrew Beers <apbeers@gmail.com>
2018-01-10 02:39:02 -0600

    get direction of startup

--- watch Extension/DataManager.swift
+++ watch Extension/DataManager.swift
@@ -18,6 +18,7 @@ struct Startup {
     var latitude: Double
     var longitude: Double
     var distance: Double
+    var direction: String
     var nearestRoad: String
 }
 
@@ -56,6 +57,7 @@ class DataManager: NSObject, WCSessionDelegate, CLLocationManagerDelegate {
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         
+        location = locationManager.location?.coordinate
         NotificationCenter.default.post(name: .StartupsUpdated, object: nil)
     }
     
@@ -106,7 +108,7 @@ class DataManager: NSObject, WCSessionDelegate, CLLocationManagerDelegate {
                         return
                 }
                 
-                self.startups.append(Startup(id: item["startup_id"].description, title: item["title"].description, brewery: item["snippet"].description, latitude: latitude, longitude: longitude, distance: 0, nearestRoad: ""))
+                self.startups.append(Startup(id: item["startup_id"].description, title: item["title"].description, brewery: item["snippet"].description, latitude: latitude, longitude: longitude, distance: 0, direction: "", nearestRoad: ""))
             }
             
             NotificationCenter.default.post(name: .StartupsUpdated, object: nil)
@@ -122,20 +124,91 @@ class DataManager: NSObject, WCSessionDelegate, CLLocationManagerDelegate {
             guard let latitude = location?.latitude, let longitude = location?.longitude else {
                 return startups
             }
-            
+ 
             let userLocation = CLLocation(latitude: latitude, longitude: longitude)
             
             for i in 0 ..< startups.count {
-                
+
                 let startupLocation = CLLocation(latitude: startups[i].latitude, longitude: startups[i].longitude)
+                startups[i].distance = startupLocation.distance(from: userLocation)
                 
-                var distance = startupLocation.distance(from: userLocation)
+                let yDiff = userLocation.coordinate.latitude - startupLocation.coordinate.latitude
+                let xDiff = userLocation.coordinate.longitude - startupLocation.coordinate.longitude
+                let angle = atan2(yDiff, xDiff) * (180 / Double.pi)
                 
-                if distance < 0 {
-                    distance = distance * -1
+                /*
+ 
+                the startups are West of the user
+                 1.23425614582588
+                 0.99121817735051
+                 1.14627456554782
+                 0.972177571669565
+                 1.06905467815322
+                 1.16739709423345
+                 1.20290043883435
+                 1.19164002386384
+ 
+                 the startups are North of the user
+                 -87.7556369342647
+                 -87.7255423299589
+                 -87.7508099560592
+                 -87.6101515487674
+                 -87.6305539378423
+                 -87.7439881669118
+                 -87.7790152261104
+                 -87.777484842513
+ 
+                 the startups are East of the user
+                 -179.852737754242
+                 -179.699908885254
+                 -179.797335923774
+                 -179.689990240963
+                 -179.751320752709
+                 -179.810857824874
+                 -179.832276574104
+                 -179.82522484983
+                 
+                 the startups are South of the user
+                 92.4614151016621
+                 92.3305194118201
+                 92.4408650023876
+                 91.8219891685688
+                 91.9132788197494
+                 92.4109043972371
+                 92.5637327675325
+                 92.5571625573206
+                */
+                
+                // -180 East -157.5 < angle < -180
+                // -135 NE -157.5 < angle < -112.5
+                // -90 North -112.5 < angle < -67.5
+                // -45 NW -67.5 < angle < -22.5
+                // 0  West -22.5 < angle < 22.5
+                // 45 SW 22.5 < angle < 67.5
+                // 90 South 67.5 < angle < 112.5
+                // 135 SE 112.5 < angle < 157.5
+                // 180 East 157.5 < angle < 180
+                
+                if -157.5 < angle && angle < -112.5 {
+                    startups[i].direction = "NE"
+                } else if -112.5 < angle && angle < -67.5 {
+                    startups[i].direction = "N"
+                } else if -67.5 < angle && angle < -22.5 {
+                    startups[i].direction = "NW"
+                } else if -22.5 < angle && angle < 22.5 {
+                    startups[i].direction = "W"
+                } else if 22.5 < angle && angle < 67.5 {
+                    startups[i].direction = "SW"
+                } else if 67.5 < angle && angle < 112.5 {
+                    startups[i].direction = "S"
+                } else if 112.5 < angle && angle < 157.5 {
+                    startups[i].direction = "SE"
+                } else if (-157.5 < angle && angle < -180) || (157.5 < angle && angle < 180) {
+                    startups[i].direction = "E"
+                } else {
+                    startups[i].direction = "X"
                 }
-                
-                startups[i].distance = distance
+                print(startups[i].direction)
             }
         }
         
