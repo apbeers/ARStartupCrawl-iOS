a86d563c6020471f0db72ae3e6205825d67fb6a7
Andrew Beers <apbeers@gmail.com>
2018-07-20 18:20:00 -0500

    add sponsors screen

---
+++ ar-startup-crawl/SponsorsViewController.swift
@@ -0,0 +1,37 @@
+//
+//  SponsorsViewController.swift
+//  ar-startup-crawl
+//
+//  Created by Andrew Beers on 7/18/18.
+//  Copyright © 2018 Andrew Beers. All rights reserved.
+//
+
+import UIKit
+
+class SponsorsViewController: UIViewController {
+
+    @IBOutlet weak var btn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
